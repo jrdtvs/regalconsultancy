96ff27b981891705e0f39192525f805aac5d34b4
jrdtvs <jrdtvs@gmail.com>
2017-08-15 12:51:38 +0530

    main page modified

--- templates/home/home.js
+++ templates/home/home.js
@@ -2,37 +2,45 @@ angular.module('regalConsultancy', ['angular-flexslider']).controller('homeCtrl'
     $scope.sliderData = [{
         index: 0,
         img: 'http://flexslider.woothemes.com/images/kitchen_adventurer_cheesecake_brownie.jpg',
-        caption: 'Australia'
+        caption: 'Australia',
+		programSupport: ['General Migrate Program', 'Federal Skilled Worker Program', 'Student Program']
     }, {
         index: 1,
         img: 'http://flexslider.woothemes.com/images/kitchen_adventurer_lemon.jpg',
-        caption: 'Canada'
+        caption: 'Canada',
+		programSupport: ['General Migrate Program', 'Federal Skilled Worker Program', 'Student Program']
     }, {
         index: 2,
         img: 'http://flexslider.woothemes.com/images/kitchen_adventurer_donut.jpg',
-        caption: 'NewZealand'
+        caption: 'NewZealand',
+		programSupport: ['General Migrate Program', 'Federal Skilled Worker Program', 'Student Program']
     }, {
         index: 3,
         img: 'http://flexslider.woothemes.com/images/kitchen_adventurer_caramel.jpg',
-        caption: 'USA'
+        caption: 'USA',
+		programSupport: ['Visitor Visa','Student Program']
     }];
 
     $scope.assesibilityData = [{
         index: 'A',
         title: 'Free Assessment of your eligibility',
-        detail: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
+        detail: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+		readMore: true
     },{
         index: 'B',
         title: 'Start to finish immigration package',
-        detail: 'It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
+        detail: 'It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+		readMore: true
     },{
         index: 'C',
         title: 'Immigration plan to become qualified',
-        detail: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using "Content here, content here", making it look like readable English.'
+        detail: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using "Content here, content here", making it look like readable English.',
+		readMore: true
     },{
         index: 'D',
         title: 'Hourly consultation (In person, phone, email)',
-        detail: 'Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for lorem ipsum will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).'
+        detail: 'Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for lorem ipsum will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).',
+		readMore: true
     }];
 
     $scope.visasType = [{
