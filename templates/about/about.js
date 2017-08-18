45589979e36355ea3ebd027019b7e5204ffab255
jrdtvs <jrdtvs@gmail.com>
2017-08-18 23:20:23 +0530

    minification handled

--- templates/about/about.js
+++ templates/about/about.js
@@ -1,3 +1,3 @@
 angular.module('regalConsultancy', []).controller('aboutCtrl', [function () {
-    console.log('about controller');
+
 }]);
--- templates/contact/contact.js
+++ templates/contact/contact.js
@@ -1,3 +1,2 @@
 angular.module('regalConsultancy', []).controller('contactCtrl', [function () {
-    console.log('contact controller');
 }]);
